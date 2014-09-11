6109b5b1c49685b66bb57e157087640a0e350c97
John Fahey <faheydesign@gmail.com>
2014-09-10 21:16:09 -0700

    adding mean

--- app/js/mean/views/mean.js
+++ app/js/mean/views/mean.js
@@ -1,3 +1,4 @@
+'use strict';
 // Form View
 var Backbone = require('backbone');
 var $ = require('jquery');
@@ -6,12 +7,25 @@ Backbone.$ = $;
 var FormView = Backbone.View.extend({
   tagName: 'div',
   events: {
-    'submit' : 'meanFunction'
+    'submit' : 'submitFunction'
   },
-  meanFunction: function(e){
-    // this.model.meanFunction();
-    console.log("submit");
+  submitFunction: function(e){
+    var num1 = this.$el.find('input[name="one"]').val();
+    var num2 = this.$el.find('input[name="two"]').val();
+    var num3 = this.$el.find('input[name="three"]').val();
+    var mean = this.meanFunction(num1,num2,num3);
+    this.$el.append('The mean is ' + mean + '<br/><br/>');
     e.preventDefault();
+  },
+  meanFunction: function(num1,num2,num3){
+    var arr   = [num1,num2,num3],
+        len   = arr.length,
+        total = 0;
+
+    for (var i = 0; i < len; i++) {
+       total += parseInt(arr[i]);
+    }
+    return total / len;
   },
   initialize: function() {
     this.render();
