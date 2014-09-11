d4a8a626ffdf69038181d97c33e55545e8d98b77
John Fahey <faheydesign@gmail.com>
2014-09-11 09:12:10 -0700

    Adding find of values of inputs and append of mean

--- app/js/mean/views/mean.js
+++ app/js/mean/views/mean.js
@@ -10,11 +10,18 @@ var FormView = Backbone.View.extend({
     'submit' : 'submitFunction'
   },
   submitFunction: function(e){
-    var num1 = this.$el.find('input[name="one"]').val();
-    var num2 = this.$el.find('input[name="two"]').val();
-    var num3 = this.$el.find('input[name="three"]').val();
+    var self = this.$el;
+    var num1 = self.find('input[name="one"]').val();
+    var num2 = self.find('input[name="two"]').val();
+    var num3 = self.find('input[name="three"]').val();
     var mean = this.meanFunction(num1,num2,num3);
-    this.$el.append('The mean is ' + mean + '<br/><br/>');
+    var median = this.median(num1,num2,num3);
+    var mode = this.mode(num1,num2,num3);
+
+    self.append('The mean is ' + mean + '<br/><br/>');
+    self.append('The median is ' + median + '<br/><br/>');
+    self.append('The mode is ' + mode + '<br/><br/>');
+
     e.preventDefault();
   },
   meanFunction: function(num1,num2,num3){
@@ -26,6 +33,52 @@ var FormView = Backbone.View.extend({
        total += parseInt(arr[i]);
     }
     return total / len;
+  },
+  median: function(num1,num2,num3){
+    var arr   = [num1,num2,num3],
+    result, first, second;
+
+    // put the numbers in order
+    arr.sort(function(a, b) {
+        return a - b;
+    });
+
+    first = ( arr.length - 1 ) / 2;
+
+    if ( arr.length % 2 === 0 ) {
+      second = first + 1;
+      result = (arr.slice(first, second) * 1 + arr.slice(second, second + 1) * 1) / 2;
+    } else {
+      result = arr[first];
+    }
+
+    return result;
+  },
+  mode: function(num1,num2,num3){
+    var arr   = [num1,num2,num3],
+    countArr = {},
+    num,
+    answer;
+
+    for (var i = 0; i < arr.length; i++) {
+      num = parseInt(arr[i]);
+      countArr[num] ? countArr[num]++ : countArr[num] = 1;
+    }
+
+    for (var prop in countArr) {
+      if(countArr.hasOwnProperty(prop)){
+        if (countArr[prop] > 1 && answer === undefined){
+          answer = prop;
+          console.log("greater than and undefined");
+        }else if (countArr[prop] > countArr[answer]){
+          answer = prop;
+          console.log("greater than and answer");
+        }else{
+          answer = "none";
+        }
+      }
+    }
+    return answer;
   },
   initialize: function() {
     this.render();
